f811767e407c5d5ecaf2bb3a7696fda545f9204f
nayefahmad <nayef.ahmad.11@gmail.com>
2018-01-26 21:40:25 -0800

    get weeknums, plot time series

--- src/2018-01-26_clean-ed-admits-data.R
+++ src/2018-01-26_clean-ed-admits-data.R
@@ -15,34 +15,55 @@ help(package="prophet")
 rm(list=ls())
 
 # source scripts: ------------
-source(here("src", "detach_package_function.R"))
 source(here("src", "ggtheme.R"))
-
+source(here("src", "weeknum_function.R"))
 
 # TODO: ------------------------------------
 # > here and lubridate packages don't seem to play well 
-# > detach_package_function doesn't work 
+
 #*******************************************
 
 
 # read ed data in: --------------------------------
+unloadNamespace("lubridate")
 df1.ed <- read_csv(here("data", "2018-01-17_daily-ed-visits-data.csv"))
 
 # here and lubridate packages don't seem to play well, so we do this later
 library("lubridate")
 
 df1.ed %<>% 
-      mutate(year = as.factor(floor_date(StartDate, 
-                               unit = "year"))) 
+      mutate(year = floor_date(StartDate,
+                               unit = "year") %>% 
+                   as.character %>% 
+                   substr(1,4) %>% 
+                   as.factor, 
+             week = floor_date(StartDate, 
+                               unit = "week") %>% 
+                   week.fn %>% 
+                   as.factor)
 
 summary(df1.ed); head(df1.ed)
-
+as.data.frame(df1.ed)
 
 # basic graphs:-------------
+unloadNamespace("lubridate")  # otherwise here package doesn't work 
+
 p1.ed.boxplot <- 
       ggplot(df1.ed, 
              aes(x=year, y=numvisits)) + 
                    geom_boxplot() + 
-      ggsave(); p1.ed.boxplot
+      stat_summary(fun.y = mean, 
+                   geom = "point") + 
+      
+      # mytheme + 
+      theme_classic() + 
+      
+      ggsave(here("output from src", 
+                  "p1_ed-boxplot.pdf")); p1.ed.boxplot
 
 
+p2.ed.time.series.by.month. <- 
+      ggplot(df1.ed,
+             aes(x=StartDate, 
+                 y=numvisits)) + 
+      geom_line(); p2.ed.time.series
