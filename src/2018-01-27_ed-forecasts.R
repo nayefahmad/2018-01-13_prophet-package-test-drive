2ac3439bb8df17bfb47145a8f481773fac90138f
nayefahmad <nayef.ahmad.11@gmail.com>
2018-01-27 20:59:38 -0800

    trying to focus on last few dates

--- src/2018-01-27_ed-forecasts.R
+++ src/2018-01-27_ed-forecasts.R
@@ -20,7 +20,8 @@ source(here("src", "2018-01-26_clean-ed-data.R"))
 
 # TODO: ------------------------------------
 # > here and lubridate packages don't seem to play well 
-# > df3 problem: why date isn't being subsetted??? 
+# > add holidays df in prophet( ); colnames holiday, ds, 
+#     lower_window, upper_window as nums 
 #*******************************************
 
 
@@ -29,18 +30,15 @@ df3.ed.subset <-  as.data.frame(df2.ed.prophet) %>%
       slice(1:3256) %>% as.data.frame
 
 max(df3.ed.subset$ds)  
-# todo: how is this "2018-01-16"??? if I subset for 9862? 
-# why do i have to set it to subset to 3256 to get 
-#     2017-11-30?? 
 
 ed.model <- prophet(df3.ed.subset)
 
 # df of forecsat horizon: 2 weeks ----------
 future <- make_future_dataframe(ed.model, 
-                                periods = 1,
+                                periods = 365,
                                 freq = "day")  
 
-tail(future,10)  # todo: doesn't seem right
+tail(future,10)  
 
 
 # predict with predict( ): -----
@@ -52,3 +50,19 @@ tail(fcast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
 
 # plot the forecast: --------
 plot(ed.model, fcast)
+# plot(fcast)  # prduces something crazy 
+
+# how to plot only last few dates? 
+latest.fcast <- select(fcast, ds, yhat)
+latest.fcast <- latest.fcast[3600:nrow(latest.fcast) , ]
+str(latest.fcast)
+
+p1.fcast <- ggplot(latest.fcast, 
+                   aes(x=ds, y=yhat)) + 
+      geom_line() + 
+      theme_classic(); p1.fcast
+
+
+# decompose time series: --------
+prophet_plot_components(ed.model, fcast)  
+# todo: flat trend since 2016?? Does that make sense? 
